17dad50c19b8bff8542351b365cdb62aefe7405c
Michael ABDALLAH-MINCIOTTI <michael.abdallah-minciotti@mail.mcgill.ca>
2016-10-10 22:20:48 -0400

    made some modifications to make test_pooling actually work

--- test_pooling.c
+++ test_pooling.c
@@ -15,10 +15,10 @@ int do_pooling(char* input_filename, char* output_filename,int num_threads)
     printf("error %u: %s\n", error, lodepng_error_text(error));
     return -1;
   }
-  long unsigned size = width * height * 4 * sizeof(unsigned char)
-  long unsigned new_size = new_width * new_height * 4 * sizeof(unsigned char)
   new_width = width/2;
   new_height = height/2;
+  long unsigned size = width * height * 4 * sizeof(unsigned char);
+  long unsigned new_size = new_width * new_height * 4 * sizeof(unsigned char);
   new_image = malloc(new_size);
 
   printf(" old_height %d, old_width %d,old size %lu \n",height,width,size);
@@ -37,8 +37,8 @@ int do_pooling(char* input_filename, char* output_filename,int num_threads)
         if(i == j) printf("max3: %d\n",max );
         if(image[4*width*i + 4*(j+1) + k]>max) max = image[4*width*i + 4*(j+1) + k];
         if(i == j) printf("max4: %d\n",max );
-        if(i==j) printf("new image written at coord %d, %d, locatin %d / %lu\n",i/2,i/2,2*width*i + 2*j + k,new_size);
-        new_image[2*width*i + 2*j + k] = max;
+        if(i==j) printf("new image written at coord %d, %d, locatiin %d / %lu\n",i/2,i/2,2*new_width*i + 2*j + k,new_size);
+        new_image[2*new_width*i + 2*j + k] = max;
       }
     }
   }
